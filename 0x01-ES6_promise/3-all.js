04b7b6d57c7fae7ade15ac8807d2d11b07420e5b
Osigelialex <osigelialex@gmail.com>
2023-06-29 22:18:00 +0100

    Formatted code

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,12 @@
-import { uploadPhoto } from "./utils";
-import { createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-      .then((values) => {
-        const { body } = values.shift();
-        const { firstName, lastName } = values.pop(); 
+    .then((values) => {
+      const { body } = values.shift();
+      const { firstName, lastName } = values.pop();
 
-        console.log(`${body} ${firstName} ${lastName}`);
-      }).catch(() => console.log('Signup system offline'));
+      console.log(`${body} ${firstName} ${lastName}`);
+    }).catch(() => console.log('Signup system offline'));
 }
