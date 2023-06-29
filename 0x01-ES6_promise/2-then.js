664570bdd21b2043e39937bfd5531d932362dedc
Osigelialex <osigelialex@gmail.com>
2023-06-29 22:21:10 +0100

    Code fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  return promise.then(() => {
     console.log('Got a response from the API');
     return {
       status: 200,
-      body: 'Success'
-    }
+      body: 'Success',
+    };
   })
-  .catch(() => new Error());
+    .catch(() => new Error());
 }
