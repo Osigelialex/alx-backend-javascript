2de6d575c06feb27ca66ffb3669645ca84b3fc3f
Osigelialex <osigelialex@gmail.com>
2023-06-30 23:02:00 +0100

    Advanced Task

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,15 @@
+import Car from './10-car'
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+    this._range = range;
+  }
+
+  cloneCar() {
+    const ClonedCar = super.constructor || super[Symbol.species];
+    return new ClonedCar();
+  }
+}
