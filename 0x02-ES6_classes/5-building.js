323c455eb426492d719d0148095875e4731a87d5
Osigelialex <osigelialex@gmail.com>
2023-06-30 20:47:38 +0100

    Fixed constructor

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Cannot Instantiate Abstract class');
-    }
-    if (this.evacuationWarningMessage !== 'function') {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
