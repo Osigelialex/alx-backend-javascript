961c4ab07a5a9d90bbafcc04a9f2c0c53db02aeb
Osigelialex <osigelialex@gmail.com>
2023-07-05 01:34:21 +0100

    Created data view

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const dataView = new Int8Array(buffer);
-
-  if (position > dataView.length - 1 || typeof position !== 'number' || typeof value !== 'number') {
+  if (position > length) {
     throw new Error('Position outside range');
   }
 
-  dataView[position] = value;
-  return buffer;
+  const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer);
+  dataView.setInt8(position, value);
+  return dataView;
 }
