ee0d8a44b67e28925fe9c2ca20ef3960c5552790
Osigelialex <osigelialex@gmail.com>
2023-07-05 01:27:05 +0100

    Add type checking

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const dataView = new Int8Array(buffer);
 
-  if (position > dataView.length - 1) {
+  if (position > dataView.length - 1 || typeof position !== 'number' || typeof value !== 'number') {
     throw new Error('Position outside range');
   }
 
