f70f2339967cab52b001725e57d79bd8d5bb89f7
Osigelialex <osigelialex@gmail.com>
2023-08-22 11:35:17 +0100

    fixing task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,35 +1,39 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-  if (!fs.existsSync(path)) {
+  try {
+    const file = fs.readFileSync(path, 'utf8').toString();
+    let lines = file.split('\r\n');
+    lines = lines.slice(1, lines.length);
+    let numberOFStudents = 0;
+    const fields = {};
+
+    for (let line of lines) {
+      if (line === '') break;
+      line = line.trim();
+      const components = line.split(',');
+      const firstName = components[0].trim();
+      const field = components[3];
+
+      if (field === undefined || firstName === undefined || field === undefined) break;
+
+      if (field in fields) {
+        fields[field].push(firstName);
+      } else {
+        fields[field] = [firstName];
+      }
+
+      numberOFStudents += 1;
+    }
+
+    console.log(`Number of students: ${numberOFStudents}`);
+    for (const field in fields) {
+      if (field) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
+    }
+  } catch (err) {
     throw new Error('Cannot load the database');
-  }
-
-  const file = fs.readFileSync(path, 'utf8').toString();
-  let lines = file.split('\r\n');
-  lines = lines.slice(1, lines.length);
-  let numberOFStudents = 0;
-  const fields = { CS: [], SWE: [] };
-
-  for (let line of lines) {
-    if (line === '') break;
-    line = line.trim();
-    const components = line.split(',');
-    const firstName = components[0].trim();
-    const field = components[3];
-
-    if (field in fields) {
-      fields[field].push(firstName);
-    }
-
-    numberOFStudents += 1;
-  }
-
-  console.log(`Number of students: ${numberOFStudents}`);
-  for (const field in fields) {
-    if (field) {
-      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-    }
   }
 };
 
