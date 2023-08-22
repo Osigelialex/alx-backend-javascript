174b8c3ba0d0890801830dab2681829370d5ea34
Osigelialex <osigelialex@gmail.com>
2023-08-22 13:01:04 +0100

    fix eslint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const http = require('http');
 
 const hostName = 'localhost';
 const port = 1245;
@@ -6,7 +6,7 @@ const port = 1245;
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
-})
+});
 
 app.listen(port, hostName);
 module.exports = app;
