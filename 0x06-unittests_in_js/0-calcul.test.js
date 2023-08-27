8dbc4c8cd2c8a2f466b110251acb6e0c58d7dfa4
Osigelialex <osigelialex@gmail.com>
2023-08-27 19:07:16 +0100

    update test suite

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,9 +3,15 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('Should round off numbers properly', () => {
-    const case1 = calculateNumber(5.5, 2.1);
-    const case2 = calculateNumber(-3.5, -3.7);
-    assert.equal(case1, 8);
-    assert.equal(case2, -7);
+    const testCase1 = calculateNumber(5.5, 2.1);
+    assert.equal(testCase1, 8);
   });
+  it('Should handle negative values properly', () => {
+    const testCase1 = calculateNumber(-3.7, -3.5);
+    assert.equal(testCase1, -7);
+  });
+  it('Should handle long floats properly', () => {
+    const testCase1 = calculateNumber(-3.999, -4.999);
+    assert.equal(testCase1, -9);
+  })
 });
